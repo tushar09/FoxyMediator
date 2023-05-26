ed3adf8943cb0ed9dd9dd53f34f3f5a6a5eed30c
Tushar <wtushar09@gmail.com>
2023-05-27 01:16:07 +0600

    new branch created.

--- app/src/main/java/com/captaindroid/foxymediator/MainActivity.java
+++ app/src/main/java/com/captaindroid/foxymediator/MainActivity.java
@@ -3,6 +3,7 @@ package com.captaindroid.foxymediator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.captaindroid.mediator.Ad;
 import com.captaindroid.mediator.Mediator;
@@ -19,26 +20,39 @@ public class MainActivity extends AppCompatActivity {
         Mediator.with(this).setTesting(true).loadInterstitialAd("5476461102469724_5478329148949586", new AdEvents() {
             @Override
             public void onAdShown(Ad ad) {
-
+                Log.e("get url", "sho");
             }
 
             @Override
             public void onAdError(Ad ad) {
+                Log.e("get url", "err");
             }
 
             @Override
             public void onAdClosed(Ad ad) {
-
+                Log.e("get url", "cl");
             }
 
             @Override
             public void onAdLoadFailed(Ad ad) {
-
+                Log.e("get url", "lo");
             }
 
             @Override
             public void onAdLoaded(Ad ad) {
                 ad.show();
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                super.onAdClicked(ad);
+                Log.e("get url", "cl");
+            }
+
+            @Override
+            public void onAdImpression(Ad ad) {
+                super.onAdImpression(ad);
+                Log.e("get url", "im");
             }
         });
     }
